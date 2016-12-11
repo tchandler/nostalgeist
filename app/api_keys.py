f9a9f2edd6cc27f19cedb1be1bf136c3388e0539
Tom Chandler <thomasjchandler@gmail.com>
2016-12-10 23:42:06 -0500

    Make api_keys environment variables

---
+++ app/api_keys.py
@@ -0,0 +1,6 @@
+import os
+
+SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
+LAST_FM_KEY = os.environ.get('LAST_FM_KEY')
+LAST_FM_SECRET = os.environ.get('LAST_FM_SECRET')
